2f227ea65a35216e14bcab1070d30a77d399f531
Matt Jadud <matt@jadud.com>
2013-10-09 19:26:31 -0400

    Adding in some suggestions from Adam for further exploration. As per email, offered freely to be used in any way; will explicitly license CC0 or PD in future commit.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: prodcons3 commstime
+all: prodcons3 commstime verytinycsp
 # Almost no difference between SEQ/PAR delta
 # with -O1. Kinda makes sense, in this context.
 CFLAGS=-O1
@@ -12,4 +12,7 @@ commstime:
 	gcc $(CFLAGS) -DUSESEQDELTA --combine -fwhole-program -I. -o commstime-seq.exe list.c tinycsp.c commstime.c
 
 prodcons3:
-	gcc $(CFLAGS) --combine -fwhole-program -I. -o pc3.exe list.c tinycsp.c prodcons3.c
+	gcc $(CFLAGS) --combine -fwhole-program -I. -o prodcons3.exe list.c tinycsp.c prodcons3.c
+
+verytinycsp:
+	clang++ -std=c++11 -stdlib=libc++ -lc++abi verytinycsp.cpp -o verytiny.exe
---
+++ verytinycsp.cpp
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <queue>
+
+// ... processes ...
+
+class Process {
+public:
+    virtual void run() = 0;
+};
+
+#define PROC(name) \
+    class name : public Process { \
+    public: \
+        int line_; \
+        name() : line_(0) {}
+
+// local variables will go here, i.e. end up as class members
+
+#define PROC_ARGS(...) \
+        void init(__VA_ARGS__) {
+
+// initialisation code will go here
+
+#define PROC_BODY \
+        } \
+        void run() { \
+            switch (line_) { \
+            case 0:
+
+// code for the proc will go here (with DESCHEDULE adding extra cases)
+
+#define PROC_END \
+            } \
+        } \
+    };
+
+// This trick courtesy of Simon Tatham:
+//   http://www.chiark.greenend.org.uk/~sgtatham/coroutines.html
+#define DESCHEDULE \
+    do { \
+        line_ = __LINE__; \
+        return; \
+        case __LINE__: \
+            ; \
+    } while (false)
+
+// ... scheduler ...
+
+std::queue<Process *> runqueue;
+
+#define SCHEDULE(proc) \
+    runqueue.push(proc)
+
+#define START_PROC(proc, ...) \
+    do { \
+        proc.init(__VA_ARGS__); \
+        SCHEDULE (&proc); \
+    } while (false)
+
+int scheduler () {
+    while (true) {
+        if (runqueue.empty()) {
+            printf("Deadlock!\n");
+            return 1;
+        }
+
+        Process *p = runqueue.front();
+        printf("Running %p\n", (long) p);
+        runqueue.pop();
+        p->run();
+    }
+}
+
+// ... channels ...
+
+class Channel {
+public:
+    Process *waiting_;
+    int *value_;
+    Channel() : waiting_(0), value_(0) {}
+};
+
+// in occam the pointer to the value is actually stored in the process...
+
+#define CHAN_WRITE(chan, value) \
+    do { \
+        if (chan->waiting_) { \
+            *(chan->value_) = value; \
+            SCHEDULE (chan->waiting_); \
+            chan->waiting_ = 0; \
+        } else { \
+            chan->value_ = &value; \
+            chan->waiting_ = this; \
+            DESCHEDULE; \
+        } \
+    } while (false)
+
+#define CHAN_READ(chan, value) \
+    do { \
+        if (chan->waiting_) { \
+            value = *(chan->value_); \
+            SCHEDULE (chan->waiting_); \
+            chan->waiting_ = 0; \
+        } else { \
+            chan->value_ = &value; \
+            chan->waiting_ = this; \
+            DESCHEDULE; \
+        } \
+    } while (false)
+
+// (Note those two are identical except for the direction the data is copied
+// once the two processes have met. You could have a direction argument...)
+
+// ... program ...
+
+// Having to list the arguments three times is pretty ugly.
+// There are probably some preprocessor gymnastics to get around that!
+
+PROC (Producer)
+    int counter;
+    int id;
+    Channel *chan;
+PROC_ARGS (int id_, Channel *chan_)
+    id = id_;
+    chan = chan_;
+PROC_BODY
+    counter = 0;
+    while (true) {
+        ++counter;
+        printf ("Producer %d sending %d\n", id, counter);
+        CHAN_WRITE (chan, counter);
+    }
+PROC_END
+
+PROC (Consumer)
+    int local;
+    int id;
+    Channel *chan;
+PROC_ARGS (int id_, Channel *chan_)
+    id = id_;
+    chan = chan_;
+PROC_BODY
+    while (true) {
+        CHAN_READ (chan, local);
+        printf ("Consumer %d received %d\n", id, local);
+    }
+PROC_END
+
+int main() {
+    Channel chan1, chan2;
+    Producer p1, p2;
+    Consumer c1, c2;
+
+    START_PROC (p1, 1, &chan1);
+    START_PROC (c1, 1, &chan1);
+    START_PROC (p2, 2, &chan2);
+    START_PROC (c2, 2, &chan2);
+
+    return scheduler();
+}
